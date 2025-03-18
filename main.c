63264ae2e8ddff3092f6b724c65e69ec2db3ecf4
Evan Teran <evan.teran@gmail.com>
2025-03-18 15:23:34 -0400

    making sure printf handles a lack of NUL terminator in the example

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(void) {
 		char buf[100];
 		size_t read = fread(buf, 1, sizeof(buf), f);
 		if (read > 0) {
-			printf("Read %zu bytes: %s\n", read, buf);
+			printf("Read %zu bytes: %.*s\n", read, (int)read, buf);
 		}
 		fclose(f);
 	} else {
